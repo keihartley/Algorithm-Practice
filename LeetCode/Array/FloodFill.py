99f6675d8db9819b708f3080a125e4252c7a5754
Kei Hartley <Clifford.kei.hartley@gmail.com>
2022-11-05 20:39:59 -0700

    Two Array Problems

---
+++ LeetCode/Array/FloodFill.py
@@ -0,0 +1,28 @@
+# An image is represented by an m x n integer grid image where image[i][j] 
+# represents the pixel value of the image. You are also given three integers 
+# sr, sc, and color. You should perform a flood fill on the image starting 
+# from the pixel image[sr][sc]. To perform a flood fill, consider the starting 
+# pixel, plus any pixels connected 4-directionally to the starting pixel of 
+# the same color as the starting pixel, plus any pixels connected 4-directionally 
+# to those pixels (also with the same color), and so on. Replace the color of 
+# all of the aforementioned pixels with color. Return the modified image after 
+# performing the flood fill.
+
+# https://leetcode.com/problems/flood-fill/?envType=study-plan&id=level-1
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        initial = image[sr][sc]
+        if initial == color: return image
+        
+        def fill(image, r, c, init, new):
+            if (r < 0 or r >= len(image) or c < 0 or c >= len(image[0]) or image[r][c] != init or image[r][c] == new): return
+            image[r][c] = new
+            
+            fill(image, r + 1, c, init, new)
+            fill(image, r - 1, c, init, new)
+            fill(image, r, c + 1, init, new)
+            fill(image, r, c - 1, init, new)
+        
+        fill(image, sr, sc, initial, color)
+        return image
