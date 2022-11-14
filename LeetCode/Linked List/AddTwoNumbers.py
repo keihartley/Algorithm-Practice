6815edbcaf28f0709d56b51eb2b64ed2775dbf93
Kei Hartley <Clifford.kei.hartley@gmail.com>
2022-11-14 01:06:28 -0800

    LinkedListProb

---
+++ LeetCode/Linked List/AddTwoNumbers.py
@@ -0,0 +1,31 @@
+# You are given two non-empty linked lists representing two non-negative integers. 
+# The digits are stored in reverse order, and each of their nodes contains a single digit. 
+# Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# https://leetcode.com/problems/add-two-numbers/
+
+import itertools
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            
+            carry, val = divmod(carry, 10)
+            print(carry, val)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
